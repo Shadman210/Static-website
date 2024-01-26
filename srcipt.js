e83163f9eb90516d7ce24cd12b77c49a81e2022f
Shadman210 <sakibsadman2525@gmail.com>
2024-01-26 23:12:04 +0600

    first commit

---
+++ srcipt.js
@@ -0,0 +1,27 @@
+var elemContainer = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-image")
+
+elemContainer.addEventListener("mouseenter", function() {
+    fixed.style.display = "block"
+})
+
+elemContainer.addEventListener("mouseleave", function() {
+    fixed.style.display = "none"
+})
+
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach((e) => {
+    e.addEventListener("mouseenter", ()=> {
+        var image = e.getAttribute("data-image")
+        console.log(image);
+        fixed.style.backgroundImage = `url(${image})`;
+    })
+})
+
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+
